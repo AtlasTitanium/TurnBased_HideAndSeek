d76a1a216151b7d2e01243d0610dbaaf8c5d7850
Atlas <AtlasTheTitan77@gmail.com>
2019-06-04 14:46:43 +0200

    small update

--- GameBuild/TurnBased_HideAndSeek/Assets/Scripts/Gameplay/GameMenu.cs
+++ GameBuild/TurnBased_HideAndSeek/Assets/Scripts/Gameplay/GameMenu.cs
@@ -2,15 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Networking;
 
 public class GameMenu : MonoBehaviour
 {
     [Header("UI elements")]
     public Button hostGame;
     public Button joinGame;
-    public InputField ipAdress;
+    public InputField serverName;
 
-    [Header("game Elements")]
+    [Header("Game Elements")]
     public GameData gameData;
 
     void Start(){
@@ -19,17 +20,59 @@ public class GameMenu : MonoBehaviour
     }
 
     void StartHost(){
-        gameData.StartServer();
+        if(serverName.text != ""){
+            StartCoroutine(CreateHost());
+        }
+        
     }
 
     void StartJoin(){
-        if(ipAdress.text != ""){
-            string[] ipAdressNumbers = ipAdress.text.Split('.');
-            if(ipAdressNumbers.Length == 4){
-                gameData.StartClient(ipAdress.text);
-            } else {
-                Debug.LogError("Error: ip is not correct - your given ip adress either contains not enough numbers, or too many");
+        if(serverName.text != ""){
+            StartCoroutine(CreateClient());
+        }
+    }
+
+    IEnumerator CreateHost(){
+        WWWForm form = new WWWForm();
+        form.AddField("name", serverName.text);
+        form.AddField("ip", IPManager.GetLocalIPAddress());
+        UnityWebRequest www = UnityWebRequest.Post("https://studenthome.hku.nl/~pepijn.kok/PHPstuff/CreateServer.php", form);
+        yield return www.SendWebRequest();
+        Debug.Log(www.downloadHandler.text);
+        if(www.downloadHandler.text == "0"){
+            gameData.StartServer();
+        }
+    }
+
+    IEnumerator CreateClient(){
+        WWWForm form = new WWWForm();
+        form.AddField("name", serverName.text);
+        UnityWebRequest www = UnityWebRequest.Post("https://studenthome.hku.nl/~pepijn.kok/PHPstuff/JoinServer.php", form);
+        yield return www.SendWebRequest();
+        string[] ipAdressNumbers = www.downloadHandler.text.Split('.');
+        if(ipAdressNumbers.Length == 4){
+            Debug.Log(www.downloadHandler.text);
+            gameData.StartClient(www.downloadHandler.text);
+        } else {
+            Debug.LogError("Error: ip is not correct - your given ip adress either contains not enough numbers, or too many");
+        }
+    }
+}
+
+
+public static class IPManager
+{
+    public static string GetLocalIPAddress()
+    {
+        var host = System.Net.Dns.GetHostEntry(System.Net.Dns.GetHostName());
+        foreach (var ip in host.AddressList)
+        {
+            if (ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+            {
+                return ip.ToString();
             }
         }
+
+        throw new System.Exception("No network adapters with an IPv4 address in the system!");
     }
 }
