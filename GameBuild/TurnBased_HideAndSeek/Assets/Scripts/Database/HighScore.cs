d52b5367416e85517c591fefa03aa17aa8c6451a
Atlas <AtlasTheTitan77@gmail.com>
2019-06-13 17:30:16 +0200

    HighScoreImplementation

---
+++ GameBuild/TurnBased_HideAndSeek/Assets/Scripts/Database/HighScore.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class HighScore : MonoBehaviour
+{
+    public Text hiderHighScore;
+    public Text seekerHighScore;
+
+    void Start(){
+        StartCoroutine(GetHighScore_Hider());
+        StartCoroutine(GetHighScore_Seeker());
+    }
+
+    //Get and Set High score (Hider)
+    IEnumerator ServerGetHighScore_Hider(){
+        UnityWebRequest www = UnityWebRequest.Get("https://studenthome.hku.nl/~pepijn.kok/PHPstuff/GetHighScore_Hider.php");
+        yield return www.SendWebRequest();
+
+        //check for errors
+        if(www.downloadHandler.text.Contains("ERROR")){
+            Debug.Log("Can't set score");
+        }
+        if(www.downloadHandler.text.Contains("Failed")){
+            Debug.Log("School servers out");
+        }
+
+        Scores myObject = JsonUtility.FromJson<Scores>(www.downloadHandler.text);
+        hiderHighScore.text = myObject.username + " has " + myObject.AmountOfWins + " wins";
+    }
+
+    IEnumerator GetHighScore_Hider(){
+        UnityWebRequest www = UnityWebRequest.Get("http://127.0.0.1/PHPstuff/GetHighScore_Hider.php");
+        yield return www.SendWebRequest();
+
+        //check for errors
+        if(www.downloadHandler.text.Contains("ERROR")){
+            Debug.Log("Can't set score");
+        }
+        if(www.downloadHandler.text.Contains("Failed")){
+            StartCoroutine(ServerGetHighScore_Hider());
+        }
+
+        Scores myObject = JsonUtility.FromJson<Scores>(www.downloadHandler.text);
+        hiderHighScore.text = myObject.username + " has " + myObject.AmountOfWins + " wins";
+    }
+
+
+    //Get and Set High score (Seeker)
+    IEnumerator ServerGetHighScore_Seeker(){
+        UnityWebRequest www = UnityWebRequest.Get("https://studenthome.hku.nl/~pepijn.kok/PHPstuff/GetHighScore_Seeker.php");
+        yield return www.SendWebRequest();
+
+        //check for errors
+        if(www.downloadHandler.text.Contains("ERROR")){
+            Debug.Log("Can't set score");
+        }
+        if(www.downloadHandler.text.Contains("Failed")){
+            Debug.Log("School servers out");
+        }
+
+        Scores myObject = JsonUtility.FromJson<Scores>(www.downloadHandler.text);
+        seekerHighScore.text = myObject.username + " has " + myObject.AmountOfWins + " wins";
+    }
+
+    IEnumerator GetHighScore_Seeker(){
+        UnityWebRequest www = UnityWebRequest.Get("http://127.0.0.1/PHPstuff/GetHighScore_Seeker.php");
+        yield return www.SendWebRequest();
+
+        //check for errors
+        if(www.downloadHandler.text.Contains("ERROR")){
+            Debug.Log("Can't set score");
+        }
+        if(www.downloadHandler.text.Contains("Failed")){
+            StartCoroutine(ServerGetHighScore_Seeker());
+        }
+
+        Scores myObject = JsonUtility.FromJson<Scores>(www.downloadHandler.text);
+        seekerHighScore.text = myObject.username + " has " + myObject.AmountOfWins + " wins";
+    }
+}
+
+public class Scores {
+    public string username;
+    public string AmountOfWins;
+}
