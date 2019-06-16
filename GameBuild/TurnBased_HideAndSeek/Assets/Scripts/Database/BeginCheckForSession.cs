6a573a34e7a9a1c90323c1356af0339289e4c36d
Atlas <AtlasTheTitan77@gmail.com>
2019-06-16 13:19:56 +0200

    SmallDatabaseScriptChange

--- GameBuild/TurnBased_HideAndSeek/Assets/Scripts/Database/BeginCheckForSession.cs
+++ GameBuild/TurnBased_HideAndSeek/Assets/Scripts/Database/BeginCheckForSession.cs
@@ -7,31 +7,29 @@ using UnityEngine.SceneManagement;
 public class BeginCheckForSession : MonoBehaviour
 {
     public string gameSelectScene;
+
+    private string serverRequest;
+    private string localRequest;
+
     private string DatabaseID;
     void Start(){
         DatabaseID = CurrentDatabaseID.Instance.id;
-        StartCoroutine(LocalCheckForConnection());
+        
+        serverRequest = "https://studenthome.hku.nl/~pepijn.kok/PHPstuff/GetUsername.php";
+        localRequest = "http://" + DatabaseID + "/PHPstuff/GetUsername.php";
+
+        StartCoroutine(CheckForConnection(localRequest));
     }
 
-    IEnumerator ServerCheckForConnection(){
-        UnityWebRequest www = UnityWebRequest.Get("https://studenthome.hku.nl/~pepijn.kok/PHPstuff/GetUsername.php");
+    IEnumerator CheckForConnection(string request){
+        UnityWebRequest www = UnityWebRequest.Get(request);
         yield return www.SendWebRequest();
         if(www.isNetworkError){
-            Debug.Log("School servers out");
-        } else 
-        if(www.downloadHandler.text.Contains("ERROR")){
-            Debug.Log("no username");
-            this.enabled = false;
-        } else {
-            SceneManager.LoadScene(gameSelectScene);
-        }
-    }
-
-    IEnumerator LocalCheckForConnection(){
-        UnityWebRequest www = UnityWebRequest.Get("http://" + DatabaseID + "/PHPstuff/GetUsername.php");
-        yield return www.SendWebRequest();
-        if(www.isNetworkError){
-            StartCoroutine(ServerCheckForConnection());
+            if(request == serverRequest){
+                Debug.Log("network error");
+            } else {
+                StartCoroutine(CheckForConnection(serverRequest));
+            }
         } else 
         if(www.downloadHandler.text.Contains("ERROR")){
             Debug.Log("no username");
